691cd9288a121d0db5dbf6d432412a9f2873977e
Akhilesh-Sonavane <113935193+Akhilesh-Sonavane@users.noreply.github.com>
2025-05-27 16:51:54 +0530

    Create program1.java

---
+++ Assignment15/program1.java
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////
+//
+//  File Name   : program1.java
+//  Description : Accept a Integer and Print 'Diagonal' Pattern
+//  Author      : Akhilesh.P.Sonavane.
+//  Date        : 22/05/2025
+//
+////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+////////////////////////////////////////////////////////////////
+// 
+//  Class Name    : Pattern
+//  Function Name : Display
+//  Description   : Inputs a Integer and returns 'Diagonal' Pattern
+//  Input         : Integer
+//  Output        : Character
+//
+////////////////////////////////////////////////////////////////
+
+class Pattern 
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i >= j)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("#\t");
+                }
+            }
+            System.out.print("\n\n");
+        }
+    }
+}
+public class program1
+{
+    public static void main(String[] args) 
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Coloumns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1,iValue2);
+
+        sobj.close();
+    }
+}
