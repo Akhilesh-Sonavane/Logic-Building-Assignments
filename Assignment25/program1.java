2c76d27793cc73dc8fc0428969c0609501a5bbb4
Akhilesh-Sonavane <113935193+Akhilesh-Sonavane@users.noreply.github.com>
2025-06-11 22:32:14 +0530

    Create program1.java

---
+++ Assignment25/program1.java
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////
+//
+//  File Name   : program1.java
+//  Description : Accept a character character array coverting it into string and returns LowerCase of that String
+//  Author      : Akhilesh.P.Sonavane.
+//  Date        : 29/05/2025
+//
+////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////
+// 
+//  Class Name    : Display
+//  Function Name : strlwrx
+//  Description   : Inputs character array coverting it into string and and returns LowerCase of that String
+//  Input         : String
+//  Output        : String
+//
+////////////////////////////////////////////////////////////////
+
+class Display
+{
+    public void strlwrx(char[] chVal, int iSize)
+    {
+        int iCnt = 0;
+        char cSmall = '\0';
+
+        System.out.println("Small case of Entered String is : ");
+
+        for(iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            if((chVal[iCnt] >= 'A') && (chVal[iCnt] <= 'Z'))
+            {
+                cSmall = (char)(chVal[iCnt] + 32);
+                System.out.print(cSmall);        
+            }
+            else
+            {
+                System.out.print(chVal[iCnt]);
+            }
+        }
+    }
+}
+
+public class program1
+{
+    public static void main(String args[])
+    {
+        int iCnt = 0;
+        char[] cValue = null;
+        String sValue = null;
+        int iLength = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String : ");
+        sValue = sobj.nextLine();
+
+        iLength = sValue.length();
+
+        cValue = new char[iLength];
+
+        for(iCnt = 0; iCnt < iLength; iCnt++)
+        {
+            cValue[iCnt] = sValue.charAt(iCnt);
+        }
+
+        Display dobj = new Display();
+
+        dobj.strlwrx(cValue, iLength);
+
+        sobj.close();
+    }
+}
